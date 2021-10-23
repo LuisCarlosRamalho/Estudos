7c9d898dce3402d16ac969bdc9dec20742abf2db
LuisCarlosRamalho <ramalho.luiscarlos@gmail.com>
2021-10-23 03:10:13 -0300

    Iniciando Animação de transição

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let btnSingIn = document.getElementById('sign-in')
-let btnSingUp = document.getElementById('sign-up')
+let btnSignIn = document.getElementById('sign-in')
+let btnSignUp = document.getElementById('sign-up')
+
+let body = document.getElementById('body')
+
+btnSignUp.addEventListener('click', function() {
+  body.className = 'sign-up-js'
+})
+
+btnSignIn.addEventListener('click', function () {
+  body.className = 'sign-in-js'
+})
